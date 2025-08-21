db18a7a27beb1c95eef56d6c247835ec0697144c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-21 14:00:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Config:
 
         # Examples
         self.examples = [
-            ['The English writer and the Afghani soldier.'],
+            ['The hand-gun was hidden in a toy box.'],
             ['It was written by members of the United Nation.'],
             [('There were more than a hundred wolves in the Tiger Basin.  It is a dangerous place '
               'after 9 p.m., especially near Lake Victoria.')]]
