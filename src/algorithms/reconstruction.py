0ab641b0a8484c6676dd6dd643be9f27d166e458
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-26 14:55:50 +0100

    Update src/algorithms/reconstruction.py: In Progress

--- src/algorithms/reconstruction.py
+++ src/algorithms/reconstruction.py
@@ -1,5 +1,22 @@
 
+import pandas as pd
+
 class Reconstruction:
 
     def __init__(self):
-        pass
+
+        self.__fields = ['word', 'tag_p', 'score', 'start', 'end']
+        self.__rename = {'tag_p': 'entity'}
+
+    def exc(self, mappings: pd.DataFrame):
+        """
+
+        :param mappings:
+        :return:
+        """
+
+
+        frame = mappings.copy()[self.__fields]
+
+        frame = frame.copy().loc[frame['score'].notna, :]
+        frame.rename(columns=self.__rename, inplace=True)
